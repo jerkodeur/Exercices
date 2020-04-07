29c298a644b35685b7e166c323fcea8d0ecfb587
JerkoMintHome <jerome.potie@gmail.com>
2020-04-07 18:25:38 +0200

    Advanced the quest to React 5, states

--- React/quests/odyssey_react-2_1/src/components/QuoteCard.js
+++ React/quests/odyssey_react-2_1/src/components/QuoteCard.js
@@ -2,19 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './QuoteCard.css'
 
-const QuoteCard = (props) => (
-    <figure className="QuoteCard">
-        <img
-            src={props.image}
-            alt={props.character} />
-        <figcaption>
-            <blockquote>
-                {props.quote}
-            </blockquote>
-            <cite>{props.character}</cite>
-        </figcaption>
-    </figure>
-)
+class QuoteCard extends React.Component {
+
+    constructor(props) {
+        super()
+        this.state = {
+            favorite: false
+        }
+    }
+
+    render() {
+        return (
+            <figure className="QuoteCard">
+                <img
+                    src={this.props.image}
+                    alt={this.props.character} />
+                <figcaption>
+                    <blockquote>
+                        {this.props.quote}
+                    </blockquote>
+                    <p>
+                        <cite>{this.props.character}</cite>
+                        <span
+                            className={this.state.favorite ? "is-favorite" : ""} onClick={event => {
+                                this.setState(this.state.favorite ? { favorite: false } : { favorite: true })
+                            }}
+                        >&#9733;
+                        </span>
+                    </p>
+                </figcaption>
+            </figure>
+        )
+    }
+
+
+}
 
 QuoteCard.propTypes = {
     image: PropTypes.string.isRequired,
