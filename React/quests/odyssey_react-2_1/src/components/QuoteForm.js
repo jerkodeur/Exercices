7c56c82b64f1b76c68d90e0d4900a395472a0aa4
JerkoMintHome <jerome.potie@gmail.com>
2020-04-08 23:24:32 +0200

    Modify test binding of  'this'

--- React/quests/odyssey_react-2_1/src/components/QuoteForm.js
+++ React/quests/odyssey_react-2_1/src/components/QuoteForm.js
@@ -8,10 +8,11 @@ class QuoteForm extends React.Component {
   constructor(props) {
     super()
     this.state = { character: '' }
-    this.handleChange = this.handleChange.bind(this)
     this.nb_left = MAX_LENGTH
   }
-  handleChange(event) {
+
+  // arrow fx for binding
+  handleChange = event => {
     const is_ok = event.target.value.length <= MAX_LENGTH
     if (is_ok) this.setState({ character: event.target.value })
     event.target.style.borderColor = is_ok ? "green" : "red"
