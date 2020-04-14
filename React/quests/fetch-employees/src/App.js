0da1f0b05c98058ed69a4da62129641718e3859d
jerkodeur <jerome.potie@gmail.com>
2020-04-15 01:07:44 +0200

    Add api data on page load with componentDidMount

--- React/quests/fetch-employees/src/App.js
+++ React/quests/fetch-employees/src/App.js
@@ -2,52 +2,32 @@ import React from 'react';
 import DisplayEmployee from './components/DisplayEmployee'
 import axios from 'axios';
 
-const sampleEmployee = {
-  gender: 'male',
-  name: {
-    first: 'Charlie',
-    last: 'Thompson',
-  },
-  location: {
-    street: {
-      number: 761,
-      name: 'Tay Street',
-    },
-    city: 'Timaru',
-    postcode: 76111,
-  },
-  email: 'charlie.thompson@example.com',
-  picture: {
-    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
-  },
-};
-
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      employee: sampleEmployee
-    }
-    this.getEmployee = this.getEmployee.bind(this);
+  state = {
+    employee: null
   }
 
-  getEmployee() {
+  getEmployee = () => {
     // Send the request
-    axios.get('https://randomuser.me/api?nat=fr')
-      // Extract the DATA from the received response
-      .then(response => response.data)
-      // Use this data to update the state
+    axios.get('https://randomuser.me/api?nat=it')
+      .then(res => res.data)
       .then(data => {
         this.setState({
-          employee: data.results[0],
+          employee: data.results[0]
         });
       });
   }
 
+  componentDidMount = () => this.getEmployee()
+
   render() {
     return (
       <div className="App">
-        <DisplayEmployee employee={this.state.employee} />
+        {this.state.employee ? (
+          <DisplayEmployee employee={this.state.employee} />
+        ) : (
+            <p>No data yet</p>
+          )}
         <button type="button" onClick={this.getEmployee}>Get employee</button>
       </div>
     );
