0c371bba1c008db66f2047eb3f4a715cc60d2f28
jerkodeur <jerome.potie@gmail.com>
2020-04-22 21:44:02 +0200

    Odyssey exercice for use React form

---
+++ React/ateliers/react-post-form/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import FormEmployee from './components/FormEmployee'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FormEmployee />
+    </div>
+  );
+}
+
+export default App;
